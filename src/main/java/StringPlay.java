febdbea74c084d216427f8b52f1c226aa3d7eef1
Chelsea <chelsea.e.rolland@gmail.com>
2020-10-20 13:53:32 -0500

    completed the maven stringUtilis exercise

--- src/main/java/StringPlay.java
+++ src/main/java/StringPlay.java
@@ -4,16 +4,26 @@ import java.util.Scanner;
 
 public class StringPlay {
     public static String capitalizeLetter (String letter) {
-        StringUtils utils = new StringUtils();
-
         //return letter.toUpperCase();
-        return letter.utils.uppercase();
+        return StringUtils.upperCase(letter);
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a string: ");
         System.out.println(capitalizeLetter("l"));
-        String letter = scan.nextLine();
-        System.out.println(capitalizeLetter(letter));
+        String input = scan.nextLine();
+        //System.out.println(capitalizeLetter(input));
+        System.out.println("You Entered: \"" + input +"\"");
+        if (StringUtils.isNumeric(input)) {
+            System.out.println("\"" + input+"\" IS a number");
+        } else {
+            System.out.println("\"" + input+"\" is NOT a number");
+        }
+
+        System.out.println("Flipped case: " + StringUtils.swapCase(input));
+
+        System.out.println("Reversed: " + StringUtils.reverse(input));
+
     }
 }
